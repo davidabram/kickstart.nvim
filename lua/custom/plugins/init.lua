b98a0e0ed04fc95f591356db5ce0548f78eb832d
David Abram <david@crocoder.dev>
2024-03-05 00:00:14 +0100

    update

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -84,10 +84,32 @@ return {
     end,
   },
   {
-    'kkoomen/vim-doge',
-    event = "BufRead",
-    config = function()
-      vim.cmd([[call doge#install()]])
+    'glacambre/firenvim',
+
+    -- Lazy load firenvim
+    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+    lazy = false,
+    build = function()
+      vim.fn["firenvim#install"](0)
     end,
   },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    config = function()
+      require('treesitter-context').setup({
+        enable = true,      -- Enable this plugin (Can be enabled/disabled later via commands)
+        max_lines = 0,      -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 20, -- Maximum number of lines to show for a single context
+        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = 'cursor',    -- Line used to calculate context. Choices: 'cursor', 'topline'
+        -- Separator between context and content. Should be a single character string, like '-'.
+        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+        separator = nil,
+        zindex = 20, -- The Z-index of the context window
+        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+      })
+    end,
+  }
 }
