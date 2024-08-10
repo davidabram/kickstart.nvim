0dec4f0abf90d701bfa4e1c183e9de39e9ba2048
David Abram <david@crocoder.dev>
2024-08-10 16:36:14 +0000

    Added harpoon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,78 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+vim.keymap.set('n', '-', vim.cmd.Ex)
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.smartindent = true
+vim.opt.list = true
+vim.opt.listchars:append 'space:⋅'
+vim.opt.listchars:append 'eol:↴'
+
+vim.opt.signcolumn = 'yes'
+vim.opt.isfname:append '@-@'
+vim.opt.wrap = false
+vim.opt.termguicolors = true
+
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
+
+vim.keymap.set('n', 'Q', '<nop>')
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+vim.opt.guicursor = {
+  -- Normal mode and Visual mode: block cursor
+  'n-v-c:block',
+  -- Insert mode: vertical bar cursor (25% width)
+  'i:ver25',
+  -- Command-line mode: underline cursor
+  'r-cr:hor20',
+  -- Replace mode: vertical bar cursor (20% width)
+  'ci-ve:ver20',
+  -- Select mode: block cursor with the 'Cursor' highlight group
+  'sm:block-Cursor',
+}
+
+return {
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup()
+      local conf = require('telescope.config').values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require('telescope.pickers')
+          .new({}, {
+            prompt_title = 'Harpoon',
+            finder = require('telescope.finders').new_table {
+              results = file_paths,
+            },
+            previewer = conf.file_previewer {},
+            sorter = conf.generic_sorter {},
+          })
+          :find()
+      end
+
+      vim.keymap.set('n', '<C-e>', function()
+        toggle_telescope(harpoon:list())
+      end, { desc = 'Open harpoon window' })
+      vim.keymap.set('n', '<leader>a', function()
+        harpoon:list():add()
+      end)
+    end,
+  },
+}
