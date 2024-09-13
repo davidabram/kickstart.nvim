2d0d642e2a84fb6e34c953d236aaf506a7394e49
David Abram <david@crocoder.dev>
2024-09-13 22:51:37 +0200

    gleam

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -25,6 +25,9 @@ vim.keymap.set('n', 'Q', '<nop>')
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
+local lspconfig = require 'lspconfig'
+lspconfig.gleam.setup {}
+
 return {
   {
     'ThePrimeagen/harpoon',
