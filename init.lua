49c477489daf9ae7d8661d93a9a038b0537d8f9c
David Abram <david@crocoder.dev>
2024-09-17 11:51:40 +0200

    copilot to nvim-cmp

--- init.lua
+++ init.lua
@@ -819,6 +819,7 @@ require('lazy').setup({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'copilot' },
         },
       }
     end,
